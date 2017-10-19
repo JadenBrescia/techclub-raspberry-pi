8b3c39e474ba6b6009f91ad58285bf33f104b680
JadenBrescia <32748744+JadenBrescia@users.noreply.github.com>
2017-10-19 15:19:12 -0400

    Update weather.js

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -8,7 +8,15 @@ const apiKey = '602d10eaa16056b9fe71e9d6693e44db';
 module.exports = function (app) {
   app.use('/weather', router);
 };
-
+let weather = new WeatherAPI(apiKey);
+weather.getWeather({
+  location: req.param('zipCode') + ',us',
+  temp_unit: 'F'
+})
+.then(response => {
+  res.response = {
+    title "Weather for ' + response.name,
+    weather: response.main.temp + 'F - ' + response.temp, */
 router.get('/:zipCode', function (req, res, next) {
-
+res.send(req.param('zipCode'));
 });
